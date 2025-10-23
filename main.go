dca71232abb46c5a625121e0dc8a55e4825c6060
Aaron S. Joyner <aaron@joyner.ws>
2025-10-23 13:26:06 -0400

    Pile on initial set of temphum sensors

--- main.go
+++ main.go
@@ -12,7 +12,37 @@ import (
 )
 
 var configs = []Sensor{
-	{Name: "storage", MAC: "F0:24:F9:97:B8:40", IpAddress: "10.0.64.10"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.10", Name: "vault"},
+	{MAC: "F0:24:F9:97:B8:40", IpAddress: "10.0.64.11", Name: "storage"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.12", Name: "boycave"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.13", Name: "theater"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.14", Name: "flex"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.15", Name: "bath-5"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.16", Name: "exercise"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.17", Name: "mechanical"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.18", Name: "linen"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.19", Name: "lavatory-2"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.20", Name: "lower-stairwell"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.21", Name: "garage-ne"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.22", Name: "garage-se"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.23", Name: "everything"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.24", Name: "kitchen"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.25", Name: "livingroom-west"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.26", Name: "master-bedroom"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.27", Name: "master-bathroom"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.28", Name: "master-shower"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.29", Name: "master-closet"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.30", Name: "lavatory"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.31", Name: "bath-2"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.32", Name: "guest-bedroom"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.33", Name: "livingroom-east"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.34", Name: "mid-stairwell"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.35", Name: "bath-3"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.36", Name: "ethan"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.37", Name: "crafts"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.38", Name: "aaron"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.39", Name: "bath-4"},
+	{MAC: "F0:24:F9:xx:xx:xx", IpAddress: "10.0.64.40", Name: "upper-stairwell"},
 }
 
 type Sensor struct {
